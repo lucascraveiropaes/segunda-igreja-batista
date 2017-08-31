4540deb2614692739685e6b65d1dc617f6abad36
Lucas Craveiro Paes <lucascraveiropaes123@gmail.com>
2017-08-31 14:02:36 -0300

    Adicionando tela de contato e adicionando tons de azul

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -62,7 +62,7 @@ jQuery(document).ready(function() {
 	/*
 	    Google maps
 	*/
-	var position = new google.maps.LatLng(45.067883, 7.687231);
+	var position = new google.maps.LatLng(-22.105819, -41.459094);
     $('.map').gmap({'center': position,'zoom': 15, 'disableDefaultUI':true, 'callback': function() {
             var self = this;
             self.addMarker({'position': this.get('map').getCenter() });	
